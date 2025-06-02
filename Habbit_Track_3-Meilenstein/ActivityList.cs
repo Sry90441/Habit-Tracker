c02e1ebd6486f9e6da18ad32b0f8d11fcdeed34e
kozubekfl100202 <kozubekfl100202@th-nuernberg.de>
2025-06-02 12:59:15 +0200

    commit

---
+++ Habbit_Track_3-Meilenstein/ActivityList.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Habbit_Track_3_Meilenstein;
+
+public class ActivityList : Activity
+{
+    List<T> acitivityList = new List<T>();
+}
